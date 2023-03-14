7058466362e8e38a409af471204baa2c716a953e
Chris Salvador <csalvador58@gmail.com>
2023-03-13 23:59:02 -0700

    working on CRUD

--- mypantry/src/pages/HomePage.jsx
+++ mypantry/src/pages/HomePage.jsx
@@ -1,14 +1,139 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import UserContext from '../store/UserContext';
+import {
+  doc,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from 'firebase/firestore';
+import db from '../db';
+import classes from '../css/HomePage.module.css';
 
 function HomePage() {
   const user = useContext(UserContext);
+  // Add a new doc
+
+  const handleAddClick = async (e) => {
+    e.preventDefault();
+
+    const pantryCollection = collection(db, 'pantry');
+    const addPantryDoc = addDoc(pantryCollection, {
+      name: 'Pasta',
+      qty: 500,
+      unit: 'g',
+    });
+
+    addPantryDoc
+      .then((data) => {
+        console.log('Added document with ID: ', data.id);
+      })
+      .catch((e) => {
+        console.error('Error adding document: ' + e);
+      });
+  };
+
+  const handleReadClick = async (e) => {
+    e.preventDefault();
+
+    const pantryQueryCollection = query(
+      collection(db, 'pantry'),
+      where('name', '==', 'pasta')
+    );
+    const pantrySnapshot = getDocs(pantryQueryCollection);
+
+    pantrySnapshot
+      .then((data) => {
+        data.forEach((doc) => {
+          console.log(doc.id, ' => ', doc.data());
+        });
+      })
+      .catch((e) => {
+        console.error('Error reading: ' + e);
+      });
+  };
+
+  const handleReadAllClick = async (e) => {
+    e.preventDefault();
+
+    const pantryQueryCollection = query(collection(db, 'pantry'));
+
+    const pantrySnapshot = getDocs(pantryQueryCollection);
+
+    pantrySnapshot
+      .then((data) => {
+        const namesArray = [];
+        data.docs.forEach((doc) => {
+          const item = doc.data();
+          namesArray.push(item.name);
+        });
+        console.log(namesArray);
+      })
+      .catch((e) => {
+        console.error('Error reading: ' + e);
+      });
+  };
+
+  const handleUpdateClick = async (e) => {
+    e.preventDefault();
+
+    const pantryCollection = collection(db, 'pantry');
+    const pantryQueryCollection = query(
+      pantryCollection,
+      where('name', '==', 'pasta')
+    );
+    const pantrySnapshot = getDocs(pantryQueryCollection);
+
+    pantrySnapshot
+      .then((data) => {
+        if (!data.empty) {
+          const pantryDocRef = doc(db, 'pantry', data.docs[0].id);
+
+          const updatePantry = updateDoc(pantryDocRef, { qty: 1, unit: 'piece' });
+          updatePantry.then(console.log('Update successful')).catch((e) => {
+            console.error('Error reading: ' + e);
+          });
+        }
+      })
+      .catch((e) => {
+        console.error('Error reading: ' + e);
+      });
+  };
+
+  const handleDeleteClick = async (e) => {
+    e.preventDefault();
+
+    const pantryCollection = collection(db, 'pantry');
+    const pantryQueryCollection = query(
+      pantryCollection,
+      where('name', '==', 'pasta')
+    );
+    const pantrySnapshot = getDocs(pantryQueryCollection);
+
+    pantrySnapshot
+      .then((data) => {
+        if (!data.empty) {
+          const pantryDocRef = doc(db, 'pantry', data.docs[0].id);
+
+          const deletePantry = deleteDoc(pantryDocRef);
+          deletePantry.then(console.log('Delete successful')).catch((e) => {
+            console.error('Error deleting: ' + e);
+          });
+        }
+      })
+      .catch((e) => {
+        console.error('Error reading: ' + e);
+      });
+  };
 
   const loginHandler = () => {
     user.updateLogin();
   };
   return (
-    <div data-testid='homepage'>
+    <div className={classes['home-container']} data-testid='homepage'>
       {!user.isLoggedIn && (
         <>
           <h1>My Pantry</h1>
@@ -21,6 +146,13 @@ function HomePage() {
         </>
       )}
       <button onClick={loginHandler}>Toggle Login</button>
+      <button onClick={handleAddClick}>Add Doc</button>
+      <button onClick={handleReadClick}>Read Doc</button>
+      <button onClick={handleReadAllClick}>
+        Read All Docs from a collection
+      </button>
+      <button onClick={handleUpdateClick}>Update Doc</button>
+      <button onClick={handleDeleteClick}>Delete Doc</button>
     </div>
   );
 }

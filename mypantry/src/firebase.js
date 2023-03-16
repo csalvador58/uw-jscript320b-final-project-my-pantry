825a6eeb23991ca928ff47407d930df0a0ab1e0b
Chris Salvador <csalvador58@gmail.com>
2023-03-16 10:05:59 -0700

    Small change

--- mypantry/src/firebase.js
+++ mypantry/src/firebase.js
@@ -18,8 +18,8 @@ import {
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  authDomain: "my-pantry-5bfbc.firebaseapp.com",
+  projectId: "my-pantry-5bfbc",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
